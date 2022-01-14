713cc69d7487fa7ea2a1888d24f3b0f819b136a0
ej-kwag <eunjeong4601@gmail.com>
2022-01-14 11:07:18 +0900

    2021-01-14 쿼리 추가

---
+++ src/main/java/kr/artbay/mybatis/ListViewService.java
@@ -0,0 +1,38 @@
+package kr.artbay.mybatis;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.artbay.common.AES;
+import kr.artbay.common.ArtBayVo;
+import kr.artbay.common.Page;
+
+@Service
+@Transactional
+public class ListViewService {
+	@Autowired
+	ArtBayMapper mapper;
+	
+	@Autowired
+	PlatformTransactionManager manager;
+	
+	@Autowired AES aes;
+	
+	TransactionStatus status;
+	Page page;
+	
+	public List<ArtBayVo> search(Page page){
+		List<ArtBayVo> list = null;
+		int totSize = mapper.totSize(page.getFindStr());
+		page.setTotSize(totSize);
+		this.page = page;
+		
+		
+		return null;
+	}
+}
